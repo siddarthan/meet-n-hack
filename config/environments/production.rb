25d7bf2a6896bdf573143a40893d50899301f972
Siddarthan Sp <spsiddhu@gmail.com>
2013-03-09 19:36:43 +0530

    added login/sign up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,9 @@ Hacker::Application.configure do
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+   config.action_mailer.raise_delivery_errors = false
+    config.action_mailer.default_url_options = { :host => 'http://meet-n-hack.herokuapp.com/' }
+
 
   # Enable threaded mode
   # config.threadsafe!
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Hacker::Application.routes.draw do
+  devise_for :users
+
   resources :programmers
 root :to => redirect("/programmers")
   # The priority is based upon order of creation:

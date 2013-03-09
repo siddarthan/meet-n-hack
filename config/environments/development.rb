f118cc4874bee0b96c35c1fbee3d880e3e8afb2c
Siddarthan Sp <spsiddhu@gmail.com>
2013-03-09 23:48:28 +0530

    set remote host's value

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,7 @@ Hacker::Application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+  
   config.cache_classes = false
 
   # Log error messages when you accidentally call methods on nil.
@@ -15,7 +16,7 @@ Hacker::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
-    config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+    config.action_mailer.default_url_options = { :host => 'http://meet-n-hack.herokuapp.com/programmers' }
 ActionMailer::Base.smtp_settings = {  
   :address              => "smtp.gmail.com",  
   :port                 => 587,  
